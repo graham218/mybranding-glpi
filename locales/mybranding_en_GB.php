b3cc9ba97fb8d658787cc6b87d26a1ad91fe35fc
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-08-29 01:19:53 +0300

    updates, on Localization

---
+++ locales/mybranding_en_GB.php
@@ -0,0 +1,5 @@
+<?php
+$LANG['mybranding']['title'] = 'My Branding';
+$LANG['mybranding']['config'] = 'My Branding Configuration';
+$LANG['mybranding']['enable_branding'] = 'Enable Branding';
+$LANG['mybranding']['sidebar_color'] = 'Sidebar Color';
